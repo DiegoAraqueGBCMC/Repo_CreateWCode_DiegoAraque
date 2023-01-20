60069a6e748bf584699dafbb490c00db9dc404fa
DiegoAraqueGBCMC <123116448+DiegoAraqueGBCMC@users.noreply.github.com>
2023-01-20 15:41:14 -0500

    Configuramos variables private
    Para que no estén a la vista del usuario

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float speed = 5.0F;
-    public float turnSpeed;
-    public float horizontalInput;
-    public float forwardInput;
+    //Inicializa variables
+    private float speed = 5.0F;
+    private float turnSpeed = 20F;
+    private float horizontalInput;
+    private float forwardInput;
     void Start()
     {
         
@@ -17,12 +18,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        //Obtenemos Inouts
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
         //Vamos a mover el vehículo hacia adelante
         //transform.Translate(0, 0, 1);
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        //Giramos el vehículo
         transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
