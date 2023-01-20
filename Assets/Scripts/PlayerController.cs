4773359c84021e8ce7324aa0cb885611a1ade935
DiegoAraqueGBCMC <123116448+DiegoAraqueGBCMC@users.noreply.github.com>
2023-01-20 15:05:37 -0500

    Ingresar Inputs
    declaramos  variable horizontalInput.
    buscamos en input manager el nombre de la varible de las teclas izquierda y derecha.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     public float speed = 5.0F;
     public float turnSpeed;
+    public float horizontalInput;
     void Start()
     {
         
@@ -15,9 +16,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
         //Vamos a mover el vehículo hacia adelante
         //transform.Translate(0, 0, 1);
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
